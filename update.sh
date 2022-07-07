b2ec889f8a5ad57e3d2ea3f86a0aec1aad3edb8d
William Douglas <william.douglas@intel.com>
2022-07-07 13:58:45 -0700

    rework update.sh for make update

--- update.sh
+++ update.sh
@@ -1,2 +1,11 @@
-git -C ~/git/sddm pull
-git -C ~/git/sddm diff v0.19.0.. > update-from-git.patch
+#!/bin/bash
+set -e -o pipefail
+
+SDDM_GIT=~/git/sddm
+
+git -C "$SDDM_GIT" pull
+git -C "$SDDM_GIT" diff v0.19.0.. > update-from-git.patch
+
+git diff --exit-code update-from-git.patch && exit
+make autospec
+make koji-nowait
